398581fda88d946d7f261c7243289e8e75abd7b6
Eduardo Vitória <eduabarraque@gmail.com>
2022-04-07 16:05:26 -0300

    img,vid and favicon

--- Lab_progamacao_frameworks/settings.py
+++ Lab_progamacao_frameworks/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'Lab_progamacao_frameworks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,10 +112,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, img)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
